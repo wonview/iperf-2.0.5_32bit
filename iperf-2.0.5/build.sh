48a09182596a77cfe1bf6074ec1dcb14834880cd
alex <alex@alex-Latitude-E5440.(none)>
2016-01-05 09:24:57 +0800

    iperf-2.0.5_32bit

---
+++ iperf-2.0.5/build.sh
@@ -0,0 +1,6 @@
+#export PATH=/home/alex/work/source/Along_A33_Android/working/android/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8/bin:$PATH
+export PATH=/home/alex/work/source/Along_A33_Android/working/lichee/out/sun8iw5p1/android/common/buildroot/external-toolchain/bin:$PATH
+./configure --host=arm-linux-gnueabi --prefix=/home/alex/work/source/Along_A33_Android/working/android/external/wpa_supplicant_8/iperf-2.0.5/bin ac_cv_func_malloc_0_nonnull=yes
+#./configure --host=arm-linux-androideabi CC=/home/alex/work/source/Along_A33_Android/working/android/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8/bin/arm-linux-androideabi-gcc CXX=/home/alex/work/source/Along_A33_Android/working/android/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.8/bin/arm-linux-androideabi-g++ --prefix=/home/alex/work/source/Along_A33_Android/working/android/external/wpa_supplicant_8/iperf-2.0.5/bin ac_cv_func_malloc_0_nonnull=yes
+#source ./configure --host=arm-none-linux-gnueabi
+#source ./configure --host=aarch64-linux-android
---
+++ iperf-2.0.5/include/iperf-int.h
@@ -0,0 +1,9 @@
+#ifndef _IPERF_INCLUDE_IPERF_INT_H
+#define _IPERF_INCLUDE_IPERF_INT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "iperf 2.0.5"
+/* generated using gnu compiler arm-linux-gnueabi-gcc (crosstool-NG linaro-1.13.1-2012.02-20120222 - Linaro GCC 2012.02) 4.6.3 20120201 (prerelease) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
